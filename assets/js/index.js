2eee5fa1b70e3738e02189592019acf746b8daa0
The-King004 <139862820+The-King004@users.noreply.github.com>
2024-02-27 23:15:01 +0100

    version 1.1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+// recuperation du hamberger-menu
+const menu = document.querySelector(".menu")
+
+// recuperation de la navlinks
+const navlinks = document.querySelector(".nav_links")
+
+// ajout d'un evenement qui fera apparaitre et disparaitre le menu
+menu.addEventListener("click", ()=>{
+    navlinks.classList.toggle("mobile-menu")
+})
